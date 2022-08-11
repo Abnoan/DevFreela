04b44b450a8bf06b02630ddfdcfdc0ae59132c28
Abnoan Muniz <Abnoan Muniz@DESKTOP-105OR1U>
2022-08-11 14:36:49 -0300

    Add project files.

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/projects")]
+    public class ProjectsController : ControllerBase
+    {
+        
+    }
+}
